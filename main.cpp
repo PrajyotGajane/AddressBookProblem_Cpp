7c62549962103afa64f30f62de704bb239da08b9
PrajyotGajane <prajyotgajane@gmail.com>
2020-09-01 23:18:42 +0530

    Added: Welcome message

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void displayWelcomeMessage()
+{
+    cout << "\n\nWelcome to Address Book Problem" << endl;
+}
+
+int main()
+{
+    displayWelcomeMessage();
+    return 0;
+}
